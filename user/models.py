595cc122a3870f3d5272c9908a0578323fc246c0
Maciej <maciej.mogilewski@gmail.com>
2022-07-10 14:27:44 +0200

    admin custom migration

--- user/models.py
+++ user/models.py
@@ -1,13 +1,1 @@
 from django.db import models
-
-
-class User(models.Model):
-    login = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-
-    class Meta:
-        verbose_name: "Custom User"
-        verbose_name_plural: "Custom Users"
-
-    def __str__(self):
-        return self.login
